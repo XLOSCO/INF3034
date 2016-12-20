65de656a2a526748f303666d05b4157929ff8112
willibill <geneve@et.esiea.fr>
2016-12-20 23:47:47 +0100

    Modif 20/12/2016

---
+++ app/src/main/java/org/esiea/geneve_losco/pong/MaTask.java
@@ -0,0 +1,14 @@
+package org.esiea.geneve_losco.pong;
+
+import java.util.Date;
+import java.util.TimerTask;
+
+/**
+ * Created by willibill on 20/12/2016.
+ */
+public class MaTask extends TimerTask {
+    @Override
+    public void run() {
+        System.out.println("ca marche");
+    }
+}
