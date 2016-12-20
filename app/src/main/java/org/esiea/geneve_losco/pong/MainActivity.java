99284f5c7c47fdb09ebc7283f56d0522c2fb3d9d
willibill <geneve@et.esiea.fr>
2016-12-20 23:40:37 +0100

    Modif 20/12/2016

--- app/src/main/java/org/esiea/geneve_losco/pong/MainActivity.java
+++ app/src/main/java/org/esiea/geneve_losco/pong/MainActivity.java
@@ -2,6 +2,8 @@ package org.esiea.geneve_losco.pong;
 
 import android.app.Activity;
 import android.content.SharedPreferences;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -16,6 +18,10 @@ import org.json.JSONException;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -23,6 +29,11 @@ public class MainActivity extends AppCompatActivity {
     int affichage;
     int hour;
     int minute;
+    Timer timer = new Timer();
+    Calendar rightNow = Calendar.getInstance();
+    int currentHour = rightNow.get(Calendar.HOUR_OF_DAY);
+    int currentMinute = rightNow.get(Calendar.MINUTE);
+    int currentSecond = rightNow.get(Calendar.SECOND);
     SharedPreferences.Editor editor;
     RecyclerView rv;
 
@@ -41,6 +52,7 @@ public class MainActivity extends AppCompatActivity {
         } return new JSONArray();
     }
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -51,6 +63,20 @@ public class MainActivity extends AppCompatActivity {
 
         hour = sp.getInt("hour", 12);
         minute = sp.getInt("minute", 0);
+
+        System.out.println(currentHour);
+        System.out.println(currentMinute);
+        System.out.println(currentSecond);
+
+        long notif_pop = ((hour - currentHour)*60*60 + (minute - currentMinute)*60 + currentSecond)*1000;
+        System.out.println(notif_pop);
+        if (notif_pop < 0) {
+            notif_pop = 86400000 + notif_pop;
+        }
+        System.out.println(notif_pop);
+
+        timer = new Timer();
+        timer.schedule(new MaTask(), notif_pop);
 
         affichage=0;
         findViewById(R.id.reglageszone).setVisibility(View.GONE);
@@ -82,6 +108,7 @@ public class MainActivity extends AppCompatActivity {
 
         final Button reglagesbutton = (Button) findViewById(R.id.reglagesbutton);
         reglagesbutton.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.M)
             public void onClick(View v) {
                 affichage=2;
                 findViewById(R.id.reglageszone).setVisibility(View.VISIBLE);
@@ -94,12 +121,19 @@ public class MainActivity extends AppCompatActivity {
 
         final Button enregbutton = (Button) findViewById(R.id.enregbutton);
         enregbutton.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.M)
             public void onClick(View v) {
                 hour=timePicker.getHour();
                 minute=timePicker.getMinute();
                 editor.putInt("hour", hour);
                 editor.putInt("minute", minute);
                 editor.commit();
+                long notif_pop = ((hour - currentHour)*60*60 + (minute - currentMinute)*60 - currentSecond)*1000;
+                if(notif_pop > 0) {
+                    notif_pop = 86400000 + notif_pop;
+                }
+                timer.schedule(new MaTask(), notif_pop);
+                System.out.println(notif_pop);
                 affichage=0;
                 findViewById(R.id.reglageszone).setVisibility(View.GONE);
                 findViewById(R.id.listezone).setVisibility(View.VISIBLE);
